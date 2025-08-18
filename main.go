8872e6cba45d33b75e2c5c165e45d88bcdf0b4c2
q <vgar@Air-vgar.IGD_Rostelecom>
2025-08-19 00:03:49 +0300

    2 homework

--- main.go
+++ main.go
@@ -2,9 +2,27 @@ package main
 
 import "fmt"
 
+const USDToEUR = 0.92
+const USDToRUB = 92.50
+const EURToRUB = USDToRUB / USDToEUR
+
+func getInput() (amount float64, from string, to string) {
+	fmt.Print("Введите сумму: ")
+	fmt.Scan(&amount)
+	fmt.Print("Введите исходную валюту (USD, EUR, RUB): ")
+	fmt.Scan(&from)
+	fmt.Print("Введите целевую валюту (USD, EUR, RUB): ")
+	fmt.Scan(&to)
+
+	return amount, from, to
+}
+
+func convert() (amount float64, from string, to string) {
+
+	return result
+}
+
 func main() {
-	const USDToEUR = 0.92
-	const USDToRUB = 92.50
-	const EURToRUB = USDToRUB / USDToEUR
+
 	fmt.Printf("1 EUR = %.2f", EURToRUB)
 }
